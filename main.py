e6866f5e7669d13b43f72a2ed1cf0bcb1044a3c0
Everton Rocha <imevertonrch@gmail.com>
2025-11-02 02:35:28 -0300

    gerando json

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import json
+
+from logger import logger
 from scraper import Scraper
 
 URL = "https://books.toscrape.com/index.html"
 
+
+def to_json(books):
+    books_data = [book.to_dict() for book in books]
+    with open("books.json", "w", encoding="utf-8") as f:
+        json.dump(books_data, f, indent=4)
+
+
 def main():
     scraper = Scraper(URL)
-    books = scraper.get_books()
+    scraper.run()
+
+    try:
+        logger.info("Starting to write JSON")
+        to_json(scraper.get_books())
+        logger.info("JSON file completed")
+    except Exception as e:
+        logger.info(f"Error while saving json: {e}")
+
 
 if __name__ == "__main__":
     main()
