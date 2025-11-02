e54a8e9af153115c24652720d0c8b68801eed9c7
Everton Rocha <imevertonrch@gmail.com>
2025-11-01 22:43:12 -0300

    capturando links das categorias

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from scraper import Scraper
+
+URL = "https://books.toscrape.com/index.html"
 
 def main():
-    pass
+    scraper = Scraper(URL)
+    scraper.init()
 
 if __name__ == "__main__":
     main()
---
+++ scraper.py
@@ -0,0 +1,31 @@
+import bs4
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+class Scraper():
+
+    def __init__(self, url):
+        self.url = url
+
+    def init(self):
+        options = Options()
+        options.add_argument("--headless")
+        with webdriver.Chrome(options=options, service=Service()) as driver:
+            try:
+                driver.get(self.url)
+
+                soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
+
+                categories = soup.find_all(class_="nav nav-list")
+
+                for category in categories:
+                    links = category.find_all("a")[1:]  # ignora primeiro link
+                    for link in links:
+                        name = link.text.strip()
+                        href = link.get("href")
+
+
+            except Exception as e:
+                pass
