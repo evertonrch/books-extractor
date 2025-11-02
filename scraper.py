553c400c99f06c56425ad11bb0dc8452d74813ec
Everton Rocha <imevertonrch@gmail.com>
2025-11-02 01:33:06 -0300

    extraindo páginas com next

--- scraper.py
+++ scraper.py
@@ -1,8 +1,12 @@
-import bs4
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
 from book import Book
+import time
 
 class Scraper():
 
@@ -12,12 +16,12 @@ class Scraper():
 
     def init(self):
         options = Options()
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         with webdriver.Chrome(options=options, service=Service()) as driver:
             try:
                 driver.get(self.url)
 
-                soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
+                soup = BeautifulSoup(driver.page_source, "html.parser")
 
                 categories = soup.find_all(class_="nav nav-list")
 
@@ -31,16 +35,28 @@ class Scraper():
 
     def process_category(self, link, driver):
         driver.get(f"https://books.toscrape.com/{link.get('href')}")
-        soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
 
-        books_section = soup.find("section").find_all("div")[1]
-        book_rows = books_section.ol.find_all("li")
-        for book in book_rows:
-            title = book.find("h3").a.text.strip()
-            price = book.find("p", class_="price_color").text
-            in_stock = True if "ok" in book.find("p", class_="instock availability").i["class"][0] else False
-            book = Book(title, price, in_stock)
-            self.books.append(book)
+        while True:
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+
+            books_div = soup.find("section").find_all("div")[1]
+            book_rows = books_div.ol.find_all("li")
+            for book in book_rows:
+                book = self.create_book_item(book)
+                self.books.append(book)
+
+            try:
+                next_button = driver.find_element(By.CSS_SELECTOR, "li.next > a")
+                next_button.click()
+                time.sleep(1)
+            except NoSuchElementException:
+                break
+
+    def create_book_item(self, book):
+        title = book.find("h3").a.text.strip()
+        price = book.find("p", class_="price_color").text
+        in_stock = True if "ok" in book.find("p", class_="instock availability").i["class"][0] else False  # verifica se tem 'ok' na classe
+        return Book(title, price, in_stock)
 
     def get_books(self):
         return self.books
